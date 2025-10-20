f163505c34bc3e829d38776877eba85f06dc0317
PolarBearEs <eloi1997@gmail.com>
2025-10-20 12:36:19 +0200

    fix project exports

--- ormodel/__init__.py
+++ ormodel/__init__.py
@@ -1,8 +1,5 @@
-# ormodel/__init__.py
-
-from sqlmodel import SQLModel, select, and_
+from sqlmodel import *
 from sqlmodel.ext.asyncio.session import AsyncSession
-
 from .base import ORModel, get_defined_models
 from .database import (
     database_context,
@@ -15,26 +12,5 @@ from .database import (
 )
 from .exceptions import DoesNotExist, MultipleObjectsReturned, SessionContextError
 from .manager import Manager, Query
-
-__all__ = [
-    "SQLModel",
-    "select",
-    "AsyncSession",
-    "ORModel",
-    "get_defined_models",
-    "database_context",
-    "db_session_context",
-    "get_engine",
-    "get_session",
-    "get_session_from_context",
-    "init_database",
-    "shutdown_database",
-    "DoesNotExist",
-    "MultipleObjectsReturned",
-    "SessionContextError",
-    "Manager",
-    "Query",
-    "and_"
-]
 
 metadata = ORModel.metadata
